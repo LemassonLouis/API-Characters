29fcb7aa05dd0f91026cb53732794dbcb94730f2
LEMASSON Louis <galou2608@gmail.com>
2023-05-22 11:51:39 +0200

    fill user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,3 +1,11 @@
 export class User {
-    
+
+    private id:Number;
+    username:String;
+    firstname:String;
+    lastname:String;
+    password:String;
+    createdAt:Date;
+    updatedAt:Date;
+
 }
