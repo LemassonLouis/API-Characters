ca2682c1e1d5006c7712179d82900f2c6a08798f
LEMASSON Louis <galou2608@gmail.com>
2023-06-12 10:21:50 +0200

    Make it work with some routes

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+
+  username :string;
+  firstname :string;
+  lastname :string;
+  password :string;
+  createdAt :Date;
+  updatedAt :Date;
+
+  constructor(
+    username :string,
+    firstname :string,
+    lastname :string,
+    password :string,
+    createdAt :Date,
+    updatedAt :Date
+  ) {
+    this.username = username;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.password = password;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+
+}
--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -2,21 +2,21 @@ import { User } from "../entities/user.entity";
 
 export class UserDto {
 
-    private id:Number;
-    username:String;
-    firstname:String;
-    lastname:String;
-    password:String;
-    createdAt:Date;
-    updatedAt:Date;
+    id :Number;
+    username :String;
+    firstname :String;
+    lastname :String;
+    password :String;
+    createdAt :Date;
+    updatedAt :Date;
 
-    constructor(user:User) {
+    constructor(user :User) {
         this.username = user.username;
         this.firstname = user.firstname;
         this.lastname = user.lastname;
         this.password = user.password;
-        this.createdAt = new Date;
-        this.updatedAt = new Date;
+        this.createdAt = user.createdAt;
+        this.updatedAt = user.updatedAt;
     }
 
 }
